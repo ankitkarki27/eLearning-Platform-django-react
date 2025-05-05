91e838bb0642e8256fd2d123a8aa9b3448d993f5
ankitkarki27 <ankitkarki8088@gmail.com>
2025-05-06 00:51:52 +0545

    feat(main): added main e-learning models

--- Backend/main/models.py
+++ Backend/main/models.py
@@ -23,6 +23,9 @@ class Course(models.Model):
     title=models.CharField(max_length=200,unique=True)
     slug=models.SlugField(max_length=200,unique=True,null=True,blank=True)
     description=models.TextField(null=True,blank=True)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
 #Enrollment model
 #LEsson Model
 #Payment model
