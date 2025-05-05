8bc2013e3a851a3bb5db73a6b024c3f476f287b5
ankitkarki27 <ankitkarki8088@gmail.com>
2025-05-06 00:49:13 +0545

    added models in main ap

--- Backend/main/models.py
+++ Backend/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import UserAccount
+from django.utils.text import slugify
 
 # Create your models here.
+#course
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return super.name
+    
+    class Meta:
+        db_table="category"
+        verbose_name_plural="Categories"
+        # order by number of courses in each category
+        ordering=["-created_at"]    
+    
+    
+class Course(models.Model):
+    title=models.CharField(max_length=200,unique=True)
+    slug=models.SlugField(max_length=200,unique=True,null=True,blank=True)
+    description=models.TextField(null=True,blank=True)
+#Enrollment model
+#LEsson Model
+#Payment model
