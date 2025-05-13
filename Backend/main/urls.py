0c0b3db8c346f4a3835e2791cefb916210bbab00
ankitkarki27 <ankitkarki8088@gmail.com>
2025-05-14 00:55:05 +0545

    feat:update the main views

--- Backend/main/urls.py
+++ Backend/main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import *
-
+from .views import (
+    CategoryViewSet, CourseViewSet, SectionViewSet,
+    CartViewSet,AttachmentViewSet, 
+    #EnrollmentViewSet, PaymentViewSet,
+    #CertificateViewSet
+)
 router= DefaultRouter()
 router.register(r"category", CategoryViewSet, basename="category")
 router.register(r"course", CourseViewSet, basename="course")
+router.register(r"cart", CartViewSet, basename="cart")
+router.register(r'sections', SectionViewSet)
+router.register(r"attachments", AttachmentViewSet, basename="attachments")
 
 urlpatterns = [
     
